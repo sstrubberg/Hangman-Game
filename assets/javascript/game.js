aceee9aa4d6ccd86542e9febf74753add402e343
Scott <sstrubberg@us.ibm.com>
2017-11-10 21:31:28 -0600

    added style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,12 @@
-var words = ["thomas", "percy", "james", "marion", "stephen", "rosie", "gordon"]
+var words = ["thomas", "percy", "james", "marion", "stephen", "rosie", "gordon", "edward", "henry", "toby", "emily", "oliver", "fergus", "whiff"]
 var randoWord = words[Math.floor(Math.random() * words.length)];
 var wordArray = randoWord.split("");
 var answerArray = [];
     for (var i = 0; i < randoWord.length; i++) {
         answerArray[i] = "__";
     }
-    
 var remainingLetters = randoWord.length;
-var guessesLeft = 3;
+var guessesLeft = 9;
 var wins = [];
 var gameStarted = false;
 var guessedLetters = [];
@@ -17,7 +16,7 @@ var score = 0;
     document.onkeyup = function(event) { // press a button and it tells you if the letter is correct
         if (gameStarted === false) {
             gameStarted = true;
-            displayResults(randoWord, "No key pressed yet!", guessedLetters, guessesLeft, answerArray);
+            displayResults("No key pressed yet!", guessedLetters, guessesLeft, answerArray);
             return
         }
 
@@ -47,8 +46,7 @@ var score = 0;
                         answerArray[i] = "__";
                     }
                 guessedLetters = [];
-                var winDiv=$("<div>");
-
+                
             }
             
         } else {
@@ -62,18 +60,17 @@ var score = 0;
             guessedLetters = [];
             guessesLeft = 3;
         }
-        displayResults(randoWord, pressedKeyMatch, guessedLetters, guessesLeft, answerArray);
+        displayResults(pressedKeyMatch, guessedLetters, guessesLeft, answerArray);
     }
 
-function displayResults(randoWord, pressedKeyMatch, guessedLetters, guessesLeft, answerArray) {
+function displayResults(pressedKeyMatch, guessedLetters, guessesLeft, answerArray) {
     answerArray.toString();
     guessedLetters.toString();
     var html =
-        "<p>Randomly Generated Word: " + randoWord + "</p>" +
         "<p>Chosen letter: " + pressedKeyMatch + "</p>" +
         "<p>Here's all the letter's you've guessed: " + guessedLetters.join(" ") + "</p>" +
         "<p>Guesses left: " + guessesLeft + "</p>" +
-        "<p>Here's 'dem blanks: " + answerArray.join(" ") + "</p>" +
+        "<p>" + answerArray.join(" ") + "</p>" +
         "<p>Score: " + score + "</p>";
     
     document.querySelector("#hangman").innerHTML = html
